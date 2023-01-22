4c3214d8193fcb7a94de98f02bbeb38cc13ca606
Riando <riando@konfigurasi.net>
2023-01-22 10:50:29 +0700

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFBA494B);
+const Color tdBlue = Color(0xFFAAC9CE);
+
+const Color tdDark = Color(0xFF1C2135);
+const Color tdLight = Color(0xFF5A6794);
+
+const Color tdBGColor = Color(0xFFF0E7DA);
